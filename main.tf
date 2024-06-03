ed51cf92b65c542d51fb6edbae9edc6a5b376295
telest0 <so7948442@gmail.com>
2024-06-03 12:04:40 +0000

    Commit k8s server with Flux

--- main.tf
+++ main.tf
@@ -1,13 +1,37 @@
 terraform {
-    backend "gcs" {
-        bucket = "osa-gke-bucket-419209"
-        prefix = "terraform/state"
-    }
+  backend "gcs" {
+    bucket = "osa-gke-bucket-419209"
+    prefix = "terraform/state"
+  }
+}
+
+module "github_repository" {
+  source                   = "github.com/den-vasyliev/tf-github-repository"
+  github_owner             = var.GITHUB_OWNER
+  github_token             = var.GITHUB_TOKEN
+  repository_name          = var.FLUX_GITHUB_REPO
+  public_key_openssh       = module.tls_private_key.public_key_openssh
+  public_key_openssh_title = "flux0"
 }
 
 module "gke_cluster" {
-  source         = "github.com/telest0/tf-google-gke-cluster"
+  source         = "git::https://github.com/telest0/tf-google-gke-cluster.git?ref=gke_auth"
   GOOGLE_REGION  = var.GOOGLE_REGION
   GOOGLE_PROJECT = var.GOOGLE_PROJECT
   GKE_NUM_NODES  = 2
+}
+
+module "tls_private_key" {
+  source    = "github.com/telest0/tf-hashicorp-tls-keys"
+  algorithm = "RSA"
+}
+
+module "flux_bootstrap" {
+  source            = "git::https://github.com/den-vasyliev/tf-fluxcd-flux-bootstrap.git?ref=gke_auth"
+  github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
+  private_key       = module.tls_private_key.private_key_pem
+  config_host       = module.gke_cluster.config_host
+  config_token      = module.gke_cluster.config_token
+  config_ca         = module.gke_cluster.config_ca
+  github_token      = var.GITHUB_TOKEN
 }
--- variables.tf
+++ variables.tf
@@ -13,3 +13,24 @@ variable "GKE_NUM_NODES" {
   default     = 2
   description = "GKE nodes number"
 }
+
+variable "FLUX_GITHUB_REPO" {
+  type        = string
+  description = "Flux GitOps repository"
+}
+
+variable "GITHUB_OWNER" {
+  type        = string
+  description = "GITHUB_OWNER"
+}
+
+variable "GITHUB_TOKEN" {
+  type        = string
+  description = "GITHUB_TOKEN"
+}
+
+variable "FLUX_GITHUB_TARGET_PATH" {
+  type        = string
+  default     = "clusters"
+  description = "Lux manifests subdirectory"
+}
